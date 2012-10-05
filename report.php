17c4c05b2398e7fc86fef90699308c7526125800
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-10-04 21:10:15 -0700

    Transactions report mockup

---
+++ report.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Bitcoin Payment Report</title>
+    <script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-1.8.1.min.js"></script>
+    <style>
+      body, input { font-family: 'Arial'; font-size: 20px; }
+      body { width: 100% }
+      #total { font-size: 32px; }
+      table { width: 100%; }
+      td, th { min-width: 200px; text-align: left; }
+    </style>
+  </head>
+  <body>
+    <div id="container">
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Address</th>
+          <th>Amount</th>
+          <th>Exchange Rate</th>
+        </tr>
+        <tr>
+          <td>2012-01-01</td>
+          <td>2134623421lkwfkjhg</td>
+          <td>500</td>
+          <td>12.40</td>
+        </tr>
+      </table>
+    </div>
+  </body>
+</html> 
