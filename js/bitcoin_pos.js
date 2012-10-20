f43781b8955b7f689a9280f72c62803d676e396d
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-10-20 14:01:03 -0700

    Clear amount field on focus

--- js/bitcoin_pos.js
+++ js/bitcoin_pos.js
@@ -58,23 +58,24 @@ $(function() {
     updateTotal();
   });
 
-  $('#owing').keyup(updateTotal);
-  $('#owing').focus();
-  $('#owing').click(function() {
+  $('#amount').keyup(updateTotal);
+  $('#amount').focus();
+  $('#amount').focus(function() {
     $('#payment').show();
     $('#received').hide();
+    $('#total').html('0');
+    $(this).val('');
   });
 
   function updateTotal() {
-    var owing = parseFloat($('#owing').val());
-    total = owing / exchange;
+    var amount = parseFloat($('#amount').val());
+    total = amount / exchange;
     total = Math.ceil(total * 10000) / 10000;
     if (!$.isNumeric(total)) total = '';
     $('#total').html(total.toString());
     doqr('bitcoin:' + address + '?amount=' + total.toString());
   }
 
-  $('input').click(function() { $(this).val(''); });
 });
 
 function getParameterByName(name) {
