64079ceaac7fb03daaf52c8446ecc8bf561fc2d1
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-10-19 23:04:43 -0700

    Make everything bigger

--- js/qr.js
+++ js/qr.js
@@ -703,8 +703,8 @@ function genframe(instring)
 
 var wd, ht, qrc;
 function setupqr(){
-    wd = 350;
-    ht = 350;
+    wd = 450;
+    ht = 450;
     mp = document.getElementById("mapcanv");
 
     var elem = document.getElementById('qrcanv');
