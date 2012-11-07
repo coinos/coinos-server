5f907fcf5b98c5be780bb0edfe830b96d068e585
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-11-06 20:59:25 -0800

    Use node and express

---
+++ node_modules/express/test.js
@@ -0,0 +1,19 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('./')
+  , app = express()
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser('foobar'));
+app.use(express.session());
+
+app.get('/', function(req, res){
+  res.send('hello');
+});
+
+app.listen(3000);
+console.log('listening on 3000');
--- ticker.php
+++
@@ -1,1 +0,0 @@
-<? echo file_get_contents("http://bitcoincharts.com/t/depthcalc.json?symbol=virtexCAD&type=ask&amount=1000&currency=true"); ?>
