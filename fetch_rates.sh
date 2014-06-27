853b59c940c85934d5dff62be642f5e4b402adae
matresstester <anthony.tsui@gmail.com>
2014-06-26 19:30:05 -0700

    Update fetch_rates.sh
    The script needs bash when run by cron

--- fetch_rates.sh
+++ fetch_rates.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 curl -k https://api.bitcoinaverage.com/exchanges/all > $DIR/rates.tmp && mv $DIR/rates.tmp $DIR/public/js/rates.json
