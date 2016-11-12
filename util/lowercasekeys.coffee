thebitcoincoop/coinos

Rename transaction keys to lowercase

util/lowercasekeys.coffee
@@ -3,3 +3,11 @@ db.keys('user:*', (err, obj) ->
   for user in obj
     db.rename(user, user.toLowerCase()) unless user is user.toLowerCase()
 )
+
+db.keys('*:transactions', (err, obj) ->
+  for user in obj
+    db.rename(user, user.toLowerCase()) unless user is user.toLowerCase()
+)
+
+console.log('All done!')
+process.exit()

coinos/coinos-server

util/lowercasekeys.coffee
@@ -8,3 +8,9 @@ db.keys('*:transactions', (err, obj) ->
   for key in obj
     db.rename(key, key.toLowerCase()) unless key is key.toLowerCase()
 )
+
+db.keys('*:transactions:*', (err, obj) ->
+  for key in obj
+    db.rename(key, key.toLowerCase()) unless key is key.toLowerCase()
+)
+