802922d9e0981fbe0756ccf415354b15922b5070
Adam Soltys <asoltys@gmail.com>
2017-10-21 12:41:31 -0700

    clean up semi-colons

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,61 +2,48 @@
   const Promise = require('bluebird')
   const db = require('../redis')
   const moment = require('moment')
+  const config = require("../config")
+  const moment = require('moment')
 
-  txid = function(transaction, user) {
+  const txid = function(transaction, user) {
     if (transaction.match(/[a-z]/i)) {
-      return transaction;
+      return transaction
     }
-    return user + ":transactions:" + transaction;
-  };
+    return user + ":transactions:" + transaction
+  }
 
   module.exports = {
     index: function(req, res) {
-      return res.render('transactions/index', {
-        user: req.params.user,
-        currency: req.session.user.currency,
-        layout: 'layout',
-        navigation: true,
-        js: (function() {
-          return global.js;
-        }),
-        css: (function() {
-          return global.css;
-        })
-      });
-    },
-    json: function(req, res) {
-      var pattern, promises, result, x;
-      result = {
+      console.log('yep')
+      const result = {
         'transactions': []
-      };
-      promises = [];
-      pattern = req.params.user;
+      }
+      const promises = []
+      const pattern = req.params.user
       if (req.session.user.username === 'admin') {
-        x = '*';
+        x = '*'
       }
       return db.keysAsync(pattern + ":transactions").then(function(keys) {
         return Promise.all(keys.map(function(key) {
-          var user;
-          user = key.substr(0, key.indexOf(':'));
+          var user
+          user = key.substr(0, key.indexOf(':'))
           return db.lrangeAsync(key, 0, -1).then(function(transactions) {
             return Promise.all(transactions.map(function(transaction) {
               return db.hgetallAsync(txid(transaction, user)).then(function(transaction) {
                 if (transaction) {
-                  transaction.user = user;
-                  return result.transactions.push(transaction);
+                  transaction.user = user
+                  return result.transactions.push(transaction)
                 }
-              });
-            }));
-          });
-        }));
+              })
+            }))
+          })
+        }))
       }).then(function() {
-        res.write(JSON.stringify(result));
-        return res.end();
-      });
+        res.send(result)
+      })
     },
     create: function(req, res) {
-      var finish;
+      var finish
       finish = function() {
         db.hgetall("user:" + req.params.user.toLowerCase(), function(err, user) {
           return res.render('transactions/notification', {
@@ -65,12 +52,13 @@
             address: req.body.address,
             txid: req.body.txid,
             js: (function() {
-              return global.js;
+              return global.js
             }),
             css: (function() {
-              return global.css;
+              return global.css
             })
           }, function(err, html) {
+<<<<<<< 20d24a2f4370e8171821cf15f67aa07ac55f08a2
             var content, from_email, helper, mail, request, sg, subject, to_email;
             helper = require('sendgrid').mail;
             from_email = new helper.Email('info@coinos.io');
@@ -79,47 +67,57 @@
             content = new helper.Content('text/html', html);
             mail = new helper.Mail(from_email, subject, to_email, content);
             sg = require('sendgrid')(process.env.SENDGRID_TOKEN);
+=======
+            var content, from_email, helper, mail, request, sg, subject, to_email
+            helper = require('sendgrid').mail
+            from_email = new helper.Email('info@coinos.io')
+            to_email = new helper.Email(user.email)
+            subject = 'Payment Received'
+            content = new helper.Content('text/html', html)
+            mail = new helper.Mail(from_email, subject, to_email, content)
+            sg = require('sendgrid')(config.sendgrid_token)
+>>>>>>> clean up semi-colons
             request = sg.emptyRequest({
               method: 'POST',
               path: '/v3/mail/send',
               body: mail.toJSON()
-            });
+            })
             return sg.API(request, function(error, response) {
-              console.log(response.statusCode);
-              console.log(response.body);
-              return console.log(response.headers);
-            });
-          });
-        });
-        res.write(JSON.stringify(req.body));
-        return res.end();
-      };
-      db.watch(req.body.txid);
+              console.log(response.statusCode)
+              console.log(response.body)
+              return console.log(response.headers)
+            })
+          })
+        })
+        res.write(JSON.stringify(req.body))
+        return res.end()
+      }
+      db.watch(req.body.txid)
       return db.exists(req.body.txid, function(err, result) {
-        var multi;
+        var multi
         if (result) {
-          return finish();
+          return finish()
         }
-        multi = db.multi();
-        multi.hmset(req.body.txid, req.body);
-        multi.rpush(req.params.user + ":transactions", req.body.txid);
+        multi = db.multi()
+        multi.hmset(req.body.txid, req.body)
+        multi.rpush(req.params.user + ":transactions", req.body.txid)
         return multi.exec(function(err, replies) {
-          return finish();
-        });
-      });
+          return finish()
+        })
+      })
     },
     update: function(req, res) {
       return db.hset(req.params.txid, 'notes', req.body.notes, function() {
-        return res.end();
-      });
+        return res.end()
+      })
     },
     "delete": function(req, res) {
       return db.del(req.params.txid, function() {
         return db.lrem(req.params.user + ":transactions", 0, req.params.txid, function() {
-          return res.end();
-        });
-      });
+          return res.end()
+        })
+      })
     }
-  };
+  }
 
-}).call(this);
+}).call(this)
