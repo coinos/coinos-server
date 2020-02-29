a620be4d76b567d153b961b18add2626b7789880
Adam Soltys <asoltys@gmail.com>
2020-02-28 21:31:32 -0800

    fix pay to user

--- queryRoutes.js
+++ queryRoutes.js
@@ -6,7 +6,6 @@ const l = console.log;
 module.exports = (app, db, emit, seen, lna, lnb) => async (req, res) => {
   let hash = req.body.payreq;
   let payreq = bolt11.decode(hash);
-  l(payreq);
   const routes = await lna.queryRoutes({ "pub_key": payreq.payeeNodeKey, "amt": payreq.satoshis });
   res.send(routes);
 };
