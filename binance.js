629701a87138e2fedb69e260bc3951cb567b322c
Adam Soltys <asoltys@gmail.com>
2020-03-09 22:30:48 -0700

    log binance error

--- binance.js
+++ binance.js
@@ -1,24 +1,30 @@
 const WebSocket = require("ws");
+const l = require("pino")();
+
 module.exports = (app, socket) => {
   const binance = new WebSocket(
     "wss://stream.binance.com:9443/ws/btcusdt@ticker"
   );
 
   binance.onmessage = async function(event) {
-    let msg = JSON.parse(event.data);
-    app.set("bid", msg.b);
-    app.set("ask", msg.a);
-    app.set("last", msg.l);
+    try {
+      let msg = JSON.parse(event.data);
+      app.set("bid", msg.b);
+      app.set("ask", msg.a);
+      app.set("last", msg.l);
 
-    let fx = app.get("fxrates");
-    if (!fx) return;
-    let rates = {};
+      let fx = app.get("fxrates");
+      if (!fx) return;
+      let rates = {};
 
-    Object.keys(fx).map(symbol => {
-      rates[symbol] = msg.a * fx[symbol];
-    });
+      Object.keys(fx).map(symbol => {
+        rates[symbol] = msg.a * fx[symbol];
+      });
 
-    app.set("rates", rates);
+      app.set("rates", rates);
+    } catch(e) {
+      l.error("binance socket error", e);
+    } 
   };
 
   return binance;
