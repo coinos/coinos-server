f324b72f007a72d25976a066f1f201b75990993f
Adam Soltys <asoltys@gmail.com>
2020-04-03 15:13:41 -0700

    return fee rate with estimate

--- routes/bitcoin/fee.js
+++ routes/bitcoin/fee.js
@@ -14,10 +14,13 @@ module.exports = async (req, res) => {
       subtractFeeFromOutputs: amount === user.balance ? [0] : []
     } 
 
-    if (feeRate) params["feeRate"] = (parseInt(feeRate) / SATS * 1000).toFixed(8);
+    if (feeRate) params["feeRate"] = (feeRate / SATS * 1000).toFixed(8);
 
     tx = await bc.fundRawTransaction(tx, params);
-    res.send({ tx });
+    let decoded = await bc.decodeRawTransaction(tx.hex);
+    /* TODO: figure out why this 0.8 multiplier seems necessary */
+    feeRate = (tx.fee * SATS / decoded.vsize) * 1000 * 0.80145;
+    res.send({ feeRate, tx });
   } catch (e) {
     l.error("bitcoin fee estimation error", e);
     return res.status(500).send(e.message);
--- routes/liquid/fee.js
+++ routes/liquid/fee.js
@@ -14,10 +14,14 @@ module.exports = async (req, res) => {
       subtractFeeFromOutputs: amount === user.balance ? [0] : []
     } 
 
-    if (feeRate) params.feeRate = (parseFloat(feeRate) / SATS * 1000).toFixed(8);
+    if (feeRate) params["feeRate"] = (feeRate / SATS * 1000).toFixed(8);
 
     tx = await lq.fundRawTransaction(tx, params);
-    res.send({ tx });
+    let decoded = await lq.decodeRawTransaction(tx.hex);
+    /* TODO: figure out why this multiplier seems necessary */
+    feeRate = (tx.fee * SATS / decoded.size) * 1000 * 0.137685;
+    l.info(decoded.weight, decoded.size, decoded.vsize, feeRate);
+    res.send({ feeRate, tx });
   } catch (e) {
     l.error("error estimating liquid fee", e);
     return res.status(500).send(e.message);
