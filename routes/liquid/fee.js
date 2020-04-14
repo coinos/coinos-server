474d96cc5c04cba55d67c53a8ff48b8468bc99a7
Adam Soltys <asoltys@gmail.com>
2020-04-13 22:04:05 -0700

    send asset support

--- routes/liquid/fee.js
+++ routes/liquid/fee.js
@@ -1,18 +1,26 @@
 module.exports = async (req, res) => {
   let { user } = req;
-  let { address, amount, feeRate } = req.body;
+  let { address, asset, amount, feeRate } = req.body;
   let tx, fee;
 
   try {
     amount = parseInt(amount);
 
-    tx = await lq.createRawTransaction([], {
-      [address]: (amount / SATS).toFixed(8)
-    });
+    tx = await lq.createRawTransaction(
+      [],
+      {
+        [address]: (amount / SATS).toFixed(8)
+      },
+      0,
+      false,
+      {
+        [address]: asset
+      }
+    );
 
     let params = {
       subtractFeeFromOutputs: amount === user.balance ? [0] : []
-    } 
+    };
 
     if (feeRate) params.feeRate = (feeRate / SATS).toFixed(8);
 
@@ -22,9 +30,9 @@ module.exports = async (req, res) => {
       await lq.walletPassphrase(config.liquid.walletpass, 300);
 
     let blinded = await lq.blindRawTransaction(tx.hex);
-    let signed = (await lq.signRawTransactionWithWallet(blinded));
+    let signed = await lq.signRawTransactionWithWallet(blinded);
     decoded = await lq.decodeRawTransaction(signed.hex);
-    feeRate = Math.round(tx.fee * SATS * 1000 / decoded.vsize);
+    feeRate = Math.round((tx.fee * SATS * 1000) / decoded.vsize);
 
     res.send({ feeRate, tx });
   } catch (e) {
--- routes/liquid/send.js
+++ routes/liquid/send.js
@@ -4,6 +4,7 @@ module.exports = async (req, res) => {
   let { user } = req;
   let {
     address,
+    asset,
     tx: { hex }
   } = req.body;
 
