5a25124279b18b772ac73dce74e46752533f5206
Adam Soltys <asoltys@gmail.com>
2020-05-01 09:47:13 -0700

    return error if no route found

--- routes/lightning/query.js
+++ routes/lightning/query.js
@@ -3,5 +3,10 @@ const bolt11 = require("bolt11");
 module.exports = async (req, res) => {
   let hash = req.body.payreq;
   let payreq = bolt11.decode(hash);
-  res.send(await lna.queryRoutes({ pub_key: payreq.payeeNodeKey, amt: payreq.satoshis }));
+
+  try {
+    res.send(await lna.queryRoutes({ pub_key: payreq.payeeNodeKey, amt: payreq.satoshis }));
+  } catch (e) {
+    res.status(500).send(e.message);
+  } 
 };
