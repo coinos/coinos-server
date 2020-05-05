a50b7185e6581c52dcf881f636ea1a4e70722852
Adam Soltys <asoltys@gmail.com>
2020-05-05 15:28:56 -0700

    catch payreq parsing error

--- routes/lightning/query.js
+++ routes/lightning/query.js
@@ -2,9 +2,9 @@ const bolt11 = require("bolt11");
 
 module.exports = async (req, res) => {
   let { amount, payreq } = req.body;
-  payreq = bolt11.decode(payreq);
 
   try {
+    payreq = bolt11.decode(payreq);
     res.send(await lna.queryRoutes({ pub_key: payreq.payeeNodeKey, amt: amount || payreq.satoshis }));
   } catch (e) {
     res.status(500).send(e.message);
