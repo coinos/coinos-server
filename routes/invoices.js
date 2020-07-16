7b44409491e984dd816b3b5353052bee21aa66e6
Adam Soltys <asoltys@gmail.com>
2020-07-16 07:50:05 -0700

    get invoice endpoint

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,24 +1,48 @@
 const { Op } = require("sequelize");
 
+app.get("/invoice", auth, async (req, res) => {
+  try {
+    const invoice = await db.Invoice.findOne({
+      where: {
+        uuid: req.query.uuid,
+      },
+    });
+
+    res.send(invoice);
+  } catch (e) {
+    l.error("couldn't find invoice", e);
+  }
+});
+
 app.post("/invoice", auth, async (req, res) => {
   const { invoice } = req.body;
   invoice.user_id = req.user.id;
 
-  l.info("creating invoice", invoice.text, req.user.username, invoice.network, invoice.amount, invoice.tip, invoice.currency, invoice.rate.toFixed(2));
+  l.info(
+    "creating invoice",
+    invoice.text,
+    req.user.username,
+    invoice.network,
+    invoice.amount,
+    invoice.tip,
+    invoice.currency,
+    invoice.rate.toFixed(2)
+  );
 
-  if (invoice.network === 'LBTC') {
-    invoice.unconfidential = (await lq.getAddressInfo(invoice.address)).unconfidential;
-  } 
+  if (invoice.network === "LBTC") {
+    invoice.unconfidential = (
+      await lq.getAddressInfo(invoice.address)
+    ).unconfidential;
+  }
 
   const exists = await db.Invoice.findOne({
     where: {
       [Op.or]: {
         address: invoice.address || "undefined",
-        text: invoice.text
-      }
-    }
+        text: invoice.text,
+      },
+    },
   });
-
 
   res.send(
     exists ? await exists.update(invoice) : await db.Invoice.create(invoice)
