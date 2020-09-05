37162b277c49ab693a26da3e4d8aa683222bd5a9
Adam Soltys <asoltys@gmail.com>
2020-09-05 02:01:32 -0400

    close socket with bogus user

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,13 @@ module.exports = {
   apps: [
     {
       name: "coinos",
+      watch: ["src"],
       script: "index.js",
-      watch: ["./"],
       env: {
         COMMON_VARIABLE: "true",
-        NODE_OPTIONS: "--require ./.pnp.js",
       },
       env_production: {
         NODE_ENV: "production",
-        NODE_OPTIONS: "--require ./.pnp.js",
       },
     },
   ],
