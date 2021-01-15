5f7a63c72883590c2fa538c5942064e6f6a13b09
Adam Soltys <asoltys@gmail.com>
2021-01-15 18:35:05 -0500

    script to watch balance

---
+++ watcher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sats=100000000
+while true
+do
+  ln=$(lncli --lnddir=/root/.lnda --network=mainnet --chain=bitcoin --rpcserver=localhost:10001 channelbalance | jr .balance)
+  bc=$(bitcoin-cli getbalance)
+  el=$(elements-cli getbalance | jr .bitcoin)
+  total=$(node -pe "$ln + $bc * $sats + $el * $sats")
+  low=$(node -pe "$total < $1")
+  echo $(date -Is) ln $ln bc $bc el $el tot $total
+  if "$low"; then 
+    echo "Balance below threshold, shutting down"
+    yarn pm2 stop 0 > /dev/null
+    exit
+  fi
+  sleep 10 
+done
