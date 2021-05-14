691bcf0b5a92afb808790974523a3ff098305782
Ran Guin <rguin@iMac.local>
2021-05-13 23:23:52 -0700

    add baseline admin router and routes

---
+++ routes/admin.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var debug = require('debug')('admin')
+
+const { v4: uuidv4 } = require('uuid')
+
+// usage:  GET '/users'
+//
+// Returns: 
+//    { users: <list of users }
+// TO FIX - change sponsor_id so that it is retrieved from current payload (NOT FROM URL)
+router.get(
+  "/users",
+  ah(async (req, res) => {
+    var users = await db.Users.findAll()
+
+    debug('users: ' + JSON.stringify(users))
+    return res.send({users: users})
+  })
+);
+
+router.get(
+  "/referrals",
+  ah(async (req, res) => {
+    var referrals = await db.Referrals.findAll()
+
+    debug('referrals: ' + JSON.stringify(referrals))
+    return res.send({referrals: referrals})
+  })
+);
+
+module.exports = router;
