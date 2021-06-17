83e4676edf8dff5bd6423e6c860f8a93cda17a79
Ran Guin <rguin@iMac.local>
2021-06-16 22:11:21 -0700

    add knexfile

---
+++ sampleconfig/knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: 'coinos',
+      user:     'tester',
+      password: 'pass'
+    }
+  },
+
+  staging: {
+    client: 'mysql2',
+    connection: {
+      database: 'coinos_stage',
+      user:     'staging_user',
+      password: 'pass'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      database: 'coinos',
+      user:     'webuser',
+      password: 'pass'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
