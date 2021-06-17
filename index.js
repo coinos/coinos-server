75149f56201dff56a2008266e69f23ae6e5f5e58
Ran Guin <rguin@iMac.local>
2021-06-16 22:08:10 -0700

    enable MIN scope

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const axios = require("axios");
-const crypto = require("crypto");
 const bodyParser = require("body-parser");
 const compression = require("compression");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const express = require("express");
 const { Op } = require("sequelize");
-const fs = require("fs");
-const persist = require("./lib/persist");
 
 ah = require("express-async-handler");
 
@@ -20,15 +17,10 @@ fail = msg => {
 };
 
 challenge = {};
+convert = {};
 logins = {};
 sessions = {};
 sockets = {};
-
-convert = persist("data/conversions.json");
-
-if (config.bitcoin) networks.push("bitcoin");
-if (config.liquid) networks.push("liquid");
-if (config.lna) networks.push("lightning");
 
 SATS = 100000000;
 toSats = n => parseInt((n * SATS).toFixed());
@@ -37,6 +29,7 @@ app = express();
 app.enable("trust proxy");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use((err, req, res, next) => {
   const details = {
     path: req.path,
@@ -62,20 +55,41 @@ app.use(compression());
 server = require("http").Server(app);
 
 require("./db");
-require("./lib/passport");
 require("./lib/utils");
 require("./lib/sockets");
-require("./lib/rates");
-require("./lib/notifications");
+require("./lib/passport");
+  
 require("./lib/register");
 require("./lib/send");
 
 require("./routes/assets");
-require("./routes/invoices");
-require("./routes/payments");
 require("./routes/info");
-require("./routes/swaps");
+  
 require("./routes/users");
+
+// Exclude plugins for minimalist version
+if (process.env.SCOPE !== 'MIN') {
+  
+  if (config.bitcoin) networks.push("bitcoin");
+  if (config.liquid) networks.push("liquid");
+  if (config.lna) networks.push("lightning");
+
+
+  require("./lib/mail");
+
+  require("./lib/rates");
+  require("./lib/notifications");
+  if (config.imap) require("./lib/mail");
+  
+  if (config.lnurl) require("./routes/lnurl");
+  if (config.mailgun) require("./routes/funding");
+
+  require("./routes/funding");
+  require("./routes/invoices");
+  require("./routes/payments");
+
+  require("./routes/swaps");
+}
 
 //  Scope based Route Handling
 
@@ -84,10 +98,6 @@ app.use('/referrals', referralsRouter)
 
 var adminRouter = require('./routes/admin.js');
 app.use('/admin', adminRouter)
-
-if (config.imap) require("./lib/mail");
-if (config.lnurl) require("./routes/lnurl");
-if (config.mailgun) require("./routes/funding");
 
 app.use((err, req, res, next) => {
   const details = {
