baa163ec0433dd71ffee19ffe247502165c8be79
Adam Soltys <asoltys@gmail.com>
2021-07-13 23:45:19 -0700

    update sample db credentials

--- sampleconfig/knexfile.js
+++ sampleconfig/knexfile.js
@@ -1,47 +1,17 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'mysql2',
     connection: {
       host: 'mariadb',
       database: 'coinos',
-      user:     'tester',
-      password: 'pass'
-    }
-  },
-
-  staging: {
-    client: 'mysql2',
-    connection: {
-      database: 'coinos_stage',
-      user:     'staging_user',
-      password: 'pass'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      user:     'root',
+      password: 'password'
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'migrations',
+      directory: 'migrations'
     }
   },
-
-  production: {
-    client: 'mysql2',
-    connection: {
-      database: 'coinos',
-      user:     'webuser',
-      password: 'pass'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
