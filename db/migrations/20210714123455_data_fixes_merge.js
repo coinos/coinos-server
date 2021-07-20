ec17ed24db4e3f9dd825166095f9f0382c35cfeb
Adam Soltys <asoltys@gmail.com>
2021-07-20 13:35:36 -0700

    update data fixes to drop invalid records

--- db/migrations/20210714123455_data_fixes_merge.js
+++ db/migrations/20210714123455_data_fixes_merge.js
@@ -1,17 +1,9 @@
 exports.up = function(knex) {
   return Promise.all([
-
-    // check with : 
-    knex.raw("update invoices as m left join accounts on accounts.id=account_id left join users on users.id = m.user_id set m.account_id=users.account_id where m.account_id is not null and accounts.id is null"),
-    
-    knex.raw("update users left join accounts on users.account_id=accounts.id left join accounts as n on n.user_id = users.id set users.account_id=n.id where account_id is not null and accounts.id is null"),
-
-    knex.raw("update payments as m left join accounts on accounts.id=account_id left join users on users.id = m.user_id set m.account_id=users.account_id where m.account_id is not null and accounts.id is null"),
-    
-    knex.raw("update orders as m left join accounts on accounts.id=m.a1_id left join users on users.id = m.user_id set m.a1_id=users.account_id where m.a1_id is not null and accounts.id is null"),
-
-    knex.raw("update orders as m left join accounts on accounts.id=m.a2_id left join users on users.id = m.user_id set m.a2_id=users.account_id where m.a2_id is not null and accounts.id is null"),
-
+    knex.raw("delete from invoices where invoices.account_id not in (select id from accounts)"),
+    knex.raw("update users set account_id = 10427 where id = 6262 and account_id = 10428");
+    knex.raw("delete from payments where account_id not in (select id from accounts)"),
+    knex.raw("delete from orders where a1_id not in (select id from accounts) or a2_id not in (select id from accounts)"),
   ])
 }
 
