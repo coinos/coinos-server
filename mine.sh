ea5b089a4bc7e72d184dbd24c9043b8101cf9f08
Adam Soltys <asoltys@gmail.com>
2021-08-02 02:06:21 -0700

    drop wallet from mine script

--- mine.sh
+++ mine.sh
@@ -3,6 +3,6 @@
 
 while true
 do
-  docker exec -it liquid elements-cli -rpcwallet=a -datadir=/config generatetoaddress 1 $(docker exec -it liquid elements-cli -rpcwallet=a -datadir=/config getnewaddress)
+  docker exec -it liquid elements-cli -datadir=/config generatetoaddress 1 $(docker exec -it liquid elements-cli -datadir=/config getnewaddress)
   sleep 8
 done
