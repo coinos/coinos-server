3e61c801fc3ca9644304c88f86adf5aa297aeef4
Adam Soltys <asoltys@gmail.com>
2021-08-30 17:49:03 -0700

    strip spaces from usernames

--- db/migrations/20210821013757_cull_usernames.js
+++ db/migrations/20210821013757_cull_usernames.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex) {
   return Promise.all([
-    knex.raw("UPDATE users SET username = REPLACE(username, ' ', '') WHERE username like '% '"), // just in case valid users have trailing spaces in their username
-    knex.raw("DELETE FROM users WHERE !(username REGEXP '^[a-zA-Z0-9\.\_\@\-]{3,32}$'"),
+    knex.raw("UPDATE users SET username = REPLACE(username, ' ', '')"), // just in case valid users have trailing spaces in their username
+    knex.raw("DELETE FROM users WHERE !(username REGEXP '^[a-zA-Z0-9\.\_\@\-]{3,32}$')"),
   ])
 };
 
