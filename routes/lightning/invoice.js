5c4b8d7ce23e7285d85e0a704e347910fb994112
Adam Soltys <asoltys@gmail.com>
2022-07-26 23:25:06 -0700

    return json for lightning invoice

--- routes/lightning/invoice.js
+++ routes/lightning/invoice.js
@@ -13,10 +13,10 @@ module.exports = ah(async (req, res) => {
         memo,
         360
       );
-      res.send(invoice.bolt11);
+      res.send({ text: invoice.bolt11 });
     } else {
       const invoice = await lnp.addInvoice({ value, memo });
-      res.send(invoice.payment_request);
+      res.send({ text: invoice.payment_request });
     }
   } catch (e) {
     l.error("problem creating invoice", e.message, e.stack);
