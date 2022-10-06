12fcb358b14e0754457d79e9103e46c6426218a8
Adam Soltys <asoltys@gmail.com>
2022-10-06 08:15:51 +0000

    increase max param length

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import fastify from "fastify";
 import pino from "pino";
 
-export default fastify({ logger: pino(), disableRequestLogging: true });
+export default fastify({ logger: pino(), disableRequestLogging: true, maxParamLength: 500 });
