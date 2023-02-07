2294c83d6cc6d0abbf3224c0cf392e9500bda365
Adam Soltys <asoltys@gmail.com>
2023-02-07 04:46:34 +0000

    fix sample config

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ docker compose up -d
 docker run -it -v $(pwd):/app --entrypoint pnpm cs i
 docker exec -it bc bitcoin-cli createwallet coinos
 docker exec -it bc bitcoin-cli rescanblockchain
-docker exec -it bc bitcoin-cli generatetoaddress 500 $(docker exec -it bitcoin bitcoin-cli getnewaddress "" "p2sh-segwit")
+docker exec -it bc bitcoin-cli generatetoaddress 500 $(docker exec -it bc bitcoin-cli getnewaddress "" "p2sh-segwit")
