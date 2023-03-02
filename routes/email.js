b61d08c745106aa6cbdff9dcee6fe561562d84b6
Adam Soltys <asoltys@gmail.com>
2023-03-01 22:40:22 -0800

    check captcha

--- routes/email.js
+++ routes/email.js
@@ -1,18 +1,38 @@
+import { bail } from "$lib/utils";
+import got from "got";
 import config from "$config";
 import sendgrid from "@sendgrid/mail";
 
 export default {
   async send({ body }, res) {
-    sendgrid.setApiKey(config.sendgrid);
-    const msg = {
-      to: "support@coinos.io",
-      from: "support@coinos.io",
-      subject: body.subject || "Email Signup",
-      text: JSON.stringify(body),
-      html: JSON.stringify(body)
-    };
+    let { token: response } = body;
+        let { recaptcha: secret } = config;
+    let { success }  = await got
+      .post("https://www.google.com/recaptcha/api/siteverify", {
+        form: {
+          secret,
+          response
+        }
+      })
+      .json();
+    
+    if (success) {
+      delete body.token;
+      sendgrid.setApiKey(config.sendgrid);
+      const msg = {
+        to: "support@coinos.io",
+        from: "support@coinos.io",
+        subject: body.subject || "Email Signup",
+        text: JSON.stringify(body),
+        html: JSON.stringify(body)
+      };
 
-      await sendgrid.send(msg);
-      res.send({ ok: true });
+        await sendgrid.send(msg);
+    res.send({ ok: true });
+
+  }
+     else {
+         bail(res, "failed captcha");
+     } 
   }
 };
