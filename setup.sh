340dc253d49cd72a9922a4b9ee8ee3fa1de9658f
Adam Soltys <asoltys@gmail.com>
2023-04-16 21:47:25 -0700

    change image used in script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cp compose.yml.sample compose.yml
 cp -r sampledata data
 sudo chown 100:100 data/nostr/data
 docker compose up -d
-docker run -it -v $(pwd):/app --entrypoint pnpm cs i
+docker run -it -v $(pwd):/app --entrypoint pnpm asoltys/coinos-server i
 docker exec -it bc bitcoin-cli createwallet coinos
 docker exec -it bc bitcoin-cli rescanblockchain
 docker exec -it bc bitcoin-cli generatetoaddress 500 $(docker exec -it bc bitcoin-cli getnewaddress "" "p2sh-segwit")
