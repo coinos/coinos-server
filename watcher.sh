07520c066d48bbfe32c3de640d09bd9400417992
Adam Soltys <asoltys@gmail.com>
2023-05-25 22:51:33 +0000

    cln api change

--- watcher.sh
+++ watcher.sh
@@ -6,9 +6,9 @@ while true
 do
   f=/home/adam/coinos-server/data/funds/$(date +"%m-%d-%y-%T").json
   docker exec -it cl lightning-cli listfunds > $f
-  lnc=$(cat $f | jr '[.channels[] | select(.state == "CHANNELD_NORMAL") | .channel_sat] | add')
-  lnw=$(cat $f | jr '[.outputs[] | .value] | add')
-  ln=$(node -pe "$lnc + $lnw")
+  lnc=$(cat $f | jr '[.channels[] | select(.state == "CHANNELD_NORMAL") | .our_amount_msat] | add')
+  lnw=$(cat $f | jr '[.outputs[] | .amount_msat] | add')
+  ln=$(node -pe "($lnc + $lnw)/1000")
   bc=$(docker exec -it bc bitcoin-cli getbalance | jr)
   total=$(node -pe "$ln + $bc * $sats")
   accts=$(curl -s https://coinos.io/api/balances | jr '.total')
