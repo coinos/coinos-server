68cbe8c74a9dc850b225a62317d8a26cf8ac82e9
Adam Soltys <asoltys@gmail.com>
2023-10-17 20:15:37 +0000

    support email in config

--- routes/email.js
+++ routes/email.js
@@ -7,50 +7,54 @@ import { SendEmailCommand } from "@aws-sdk/client-ses";
 
 export default {
   async send({ body }, res) {
-    let { token: response } = body;
-    let { recaptcha: secret } = config;
-    let { success } = await got
-      .post("https://www.google.com/recaptcha/api/siteverify", {
-        form: {
-          secret,
-          response
-        }
-      })
-      .json();
-
-    if (success || body.token === config.adminpass) {
-      delete body.token;
-
-      let client = new SESClient({ region: "us-east-2" });
-      await client.send(
-        new SendEmailCommand({
-          Destination: {
-            CcAddresses: [],
-            ToAddresses: ["support@coinos.io"]
+    try {
+      let { token: response } = body;
+      let { recaptcha: secret } = config;
+      let { success } = await got
+        .post("https://www.google.com/recaptcha/api/siteverify", {
+          form: {
+            secret,
+            response,
           },
-          Message: {
-            Body: {
-              Html: {
-                Charset: "UTF-8",
-                Data: JSON.stringify(body)
-              },
-              Text: {
-                Charset: "UTF-8",
-                Data: JSON.stringify(body)
-              }
-            },
-            Subject: {
-              Charset: "UTF-8",
-              Data: body.subject || "Support Request"
-            }
-          },
-          Source: "support@coinos.io"
         })
-      );
+        .json();
 
-      res.send({ ok: true });
-    } else {
-      bail(res, "failed captcha");
+      if (success || body.token === config.adminpass) {
+        delete body.token;
+
+        let client = new SESClient({ region: "us-east-2" });
+        await client.send(
+          new SendEmailCommand({
+            Destination: {
+              CcAddresses: [],
+              ToAddresses: [config.support],
+            },
+            Message: {
+              Body: {
+                Html: {
+                  Charset: "UTF-8",
+                  Data: JSON.stringify(body),
+                },
+                Text: {
+                  Charset: "UTF-8",
+                  Data: JSON.stringify(body),
+                },
+              },
+              Subject: {
+                Charset: "UTF-8",
+                Data: body.subject || "Support Request",
+              },
+            },
+            Source: config.support,
+          })
+        );
+
+        res.send({ ok: true });
+      } else {
+        bail(res, "failed captcha");
+      }
+    } catch (e) {
+      console.log(e);
     }
-  }
+  },
 };
