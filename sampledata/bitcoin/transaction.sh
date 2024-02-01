ed81dd29914053276e58b961c97f7fce0ac85cf1
Adam Soltys <asoltys@gmail.com>
2024-02-01 11:08:42 -0800

    update sample transaction script

--- sampledata/bitcoin/transaction.sh
+++ sampledata/bitcoin/transaction.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget -O- --post-data='{"txid": "'$1'", "wallet": "'$2'"}' --header='Content-Type:application/json' 'http://app:3119/bitcoin'
+wget -O- --post-data='{"txid": "'$1'", "wallet": "'$2'", "type": "bitcoin"}' --header='Content-Type:application/json' 'http://app:3119/confirm'
