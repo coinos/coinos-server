091253441cb06ae75ddaceb5a3efa2376526328e
ndungudedan <dnkibere@gmail.com>
2024-10-02 23:38:27 +0300

    Initial server setup fixes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ cp compose.yml.sample compose.yml
 cp -r sampledata data
 sudo chown 100:100 data/nostr/data
 docker compose up -d
-docker run -it -v $(pwd):/app --entrypoint bun asoltys/coinos-server i
+docker run -it -v $(pwd):/home/bun/app --entrypoint bun asoltys/coinos-server i
 docker exec -it bc bitcoin-cli createwallet coinos
 docker exec -it bc bitcoin-cli rescanblockchain
 docker exec -it bc bitcoin-cli generatetoaddress 500 $(docker exec -it bc bitcoin-cli getnewaddress "" "p2sh-segwit")
+docker exec -it lq elements-cli createwallet coinos
