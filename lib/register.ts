99abcd90e594e1ff2fc22b75053b8a29429fda14
Adam Soltys <adam@coinos.io>
2025-02-25 09:44:15 -0800

    allow spaces

--- lib/register.ts
+++ lib/register.ts
@@ -10,7 +10,7 @@ import { encrypt as nip49encrypt } from "nostr-tools/nip49";
 import { authenticator } from "otplib";
 import { v4 } from "uuid";
 
-const valid = /^[\p{L}\p{N}]{2,24}$/u;
+const valid = /^[\p{L}\p{N} ]{2,24}$/u;
 export default async (user, ip) => {
   let { password, pubkey, username } = user;
   l("registering", username);
