e81de049062777873770ed45559ddc648b176476
Adam Soltys <adam@coinos.io>
2025-05-16 15:07:06 -0700

    update locales

--- lib/locales/index.ts
+++ lib/locales/index.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-export default "de,en,fa,ja,ru,el,es,fr,pt,zh".split(",").reduce((a, l) => {
+export default "ar,bn,de,el,en,es,fa,fr,hi,it,ja,ko,nl,pl,pt,ru,th,tr,zh".split(",").reduce((a, l) => {
       a[l] = JSON.parse(fs.readFileSync(`lib/locales/${l}.json`));
     return a;
 }, {});
